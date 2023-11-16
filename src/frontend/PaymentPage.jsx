77a514df3e532df08d679737b2f07a085f845921
kkamanga <144144430+kkamanga@users.noreply.github.com>
2023-11-15 22:49:39 -0500

    Update PaymentPage.jsx

--- src/frontend/PaymentPage.jsx
+++ src/frontend/PaymentPage.jsx
@@ -6,27 +6,44 @@ const PaymentPage = () => {
   const [cardNumber, setCardNumber] = useState('');
   const [expiryDate, setExpiryDate] = useState('');
   const [cvv, setCVV] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [paymentSuccessMessage, setPaymentSuccessMessage] = useState('');
 
   const handleCardNumberChange = (e) => {
-    const newCardNumber = e.target.value.replace(/[^0-9]/g, '').slice(0, 19);
+    const newCardNumber = e.target.value.slice(0, 19);
     setCardNumber(newCardNumber);
   };
 
   const handleExpiryDateChange = (e) => {
-  const newExpiryDate = e.target.value.replace(/[^0-9]/g, '');
-  const isValidDate = newExpiryDate.length === 4 && Number(newExpiryDate.slice(0, 2)) <= 12;
-  setExpiryDate(isValidDate ? newExpiryDate : '');
-};
-
+    const newExpiryDate = e.target.value;
+    const isValidDate =
+      newExpiryDate.length === 4 &&
+      Number(newExpiryDate.slice(0, 2)) >= 1 &&
+      Number(newExpiryDate.slice(0, 2)) <= 12;
+    setExpiryDate(isValidDate ? newExpiryDate : '');
+  };
 
   const handleCVVChange = (e) => {
-    const newCVV = e.target.value.replace(/[^0-9]/g, '').slice(0, 4);
+    const newCVV = e.target.value.slice(0, 4);
     setCVV(newCVV);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-9
+
+    if (cardNumber === '' || expiryDate === '' || cvv === '') {
+      setErrorMessage('Invalid entry. Please check your input.');
+      setPaymentSuccessMessage(''); // Clear success message if there was an error
+      return;
+    }
+
+    // Clear any previous error message
+    setErrorMessage('');
+
+    // Display success message
+    setPaymentSuccessMessage('Payment successful!');
+
+    // Continue with processing the valid data
     console.log('Card Number:', cardNumber);
     console.log('Expiry Date:', expiryDate);
     console.log('CVV:', cvv);
@@ -37,6 +54,8 @@ const PaymentPage = () => {
       <div className="payment-title">
         <h1 className="payment-text">Payment Information</h1>
       </div>
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
+      {paymentSuccessMessage && <div className="success-message">{paymentSuccessMessage}</div>}
       <form onSubmit={handleSubmit}>
         <div className="payment-inputs">
           <div className="payment-input">
@@ -52,7 +71,7 @@ const PaymentPage = () => {
             <input
               type="text"
               placeholder="Expiry Date (MM/YY)"
-              value={expiryDate.replace(/(\d{2})(\d{2})/, '$1/$2')}
+              value={expiryDate}
               onChange={handleExpiryDateChange}
               required
             />
